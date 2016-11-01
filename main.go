dc06fe5e75c775635711ff93cc4f0623e8454627
Jeremiah Orem <oremj@oremj.com>
2016-11-01 16:22:55 -0700

    add region detection, user@ support, env passing

--- main.go
+++ main.go
@@ -5,21 +5,34 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"syscall"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 var command = flag.String("c", "", "command to run, if set")
-var ec2Sess = ec2.New(session.Must(session.NewSession()))
+
+var awsSess *session.Session
+var ec2Sess *ec2.EC2
 
 func init() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [-c command] instance-id [ssh-args]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s [-c command] [user@]instance-id [ssh-args]\n", os.Args[0])
 		flag.PrintDefaults()
 	}
+
+	awsSess := session.Must(session.NewSession())
+
+	meta := ec2metadata.New(awsSess)
+	if region, _ := meta.Region(); region != "" {
+		awsSess = awsSess.Copy(&aws.Config{Region: aws.String(region)})
+	}
+
+	ec2Sess = ec2.New(awsSess)
 }
 
 func getInstanceHostname(instanceID string) string {
@@ -34,8 +47,12 @@ func getInstanceHostname(instanceID string) string {
 		panic(err)
 	}
 
-	if len(res.Reservations) != 1 {
-		log.Fatal("Reservations is not 1")
+	if len(res.Reservations) < 1 {
+		log.Fatal("Instance not found.")
+	}
+
+	if len(res.Reservations) > 1 {
+		log.Fatal("Too many reservations found.")
 	}
 
 	if len(res.Reservations[0].Instances) != 1 {
@@ -45,20 +62,33 @@ func getInstanceHostname(instanceID string) string {
 	return *res.Reservations[0].Instances[0].PublicDnsName
 }
 
+func parseHost(host string) (string, string) {
+	parts := strings.SplitN(host, "@", 2)
+	if len(parts) == 1 {
+		return "", parts[0]
+	}
+	return parts[0] + "@", parts[1]
+}
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
-	instanceID := args[0]
+	if len(args) < 1 {
+		flag.Usage()
+		log.Fatal("instance-id is required")
+	}
+
+	user, instanceID := parseHost(args[0])
 	sshArgs := args[1:]
 
 	hostName := getInstanceHostname(instanceID)
 
 	sshArgv := append([]string{"ssh"}, sshArgs...)
-	sshArgv = append(sshArgv, hostName)
+	sshArgv = append(sshArgv, user+hostName)
 	if *command != "" {
 		sshArgv = append(sshArgv, *command)
 	}
-	if err := syscall.Exec("/usr/bin/ssh", sshArgv, []string{}); err != nil {
+	if err := syscall.Exec("/usr/bin/ssh", sshArgv, os.Environ()); err != nil {
 		panic(err)
 	}
 }
